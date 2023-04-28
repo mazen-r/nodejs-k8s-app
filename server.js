a2ba5db4351f24a21f35bd4f09eb0122a1db97e7
Mazen Ramadan <mazenramadan00@gmail.com>
2023-04-29 01:19:13 +0300

    Add: connecting to postgres

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
+
+const connectDB = require('./database/connections')
 const { PORT } = require('./config')
 
-const startServer = () => {
+const startServer = async () => {
+    connectDB();
     const app = express();
-
     app.listen(PORT, () => {
         console.log(`App listening on port ${PORT}`)
     }) 
