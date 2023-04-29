709b75acc822542370f3423806f39e58f51711a6
Mazen Ramadan <mazenramadan00@gmail.com>
2023-04-29 18:38:24 +0300

    Add uncaught routes handler

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.use(bodyParser.json());
 
 app.use("/users", userRoute);
 
+app.use((req, res, next) => {  
+    res.status(404).json({error: "Coudln't find this URL!"});
+});
+
 app.use((err, req, res, next) => {
   res.status(500).json({error: 'Internal server error!'});
 });
