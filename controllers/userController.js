d6e6152392d2535e7a4be7ef4812e3ea6c73868a
Mazen Ramadan <mazenramadan00@gmail.com>
2023-04-30 21:14:39 +0300

    Remove unncessary route

--- controllers/userController.js
+++ controllers/userController.js
@@ -67,22 +67,4 @@ const deleteUser = async (req, res, next) => {
     }
 }
 
-const getUsers = async (req, res, next) => {
-    const id = req.params.id || 1
-    const offset = (id - 1) * 10;
-    try {
-        const usersData = await User.findAll({offset, limit:10});
-        if (usersData.length > 0) {
-            const filteredUsers = usersData.map(({ userId, userName, email, createdAt }) => ({
-                userId, userName, email, createdAt,
-            }));
-             res.status(200).json({ data: filteredUsers });
-        } else {
-            res.status(404).json({ message: "No data avaiable" });
-        }
-    } catch (err) {
-        next(err);
-    }
-};
-
-module.exports = { getUsers, profile, registerUser, loginUser, deleteUser };
+module.exports = { profile, registerUser, loginUser, deleteUser };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,6 +9,5 @@ router.post("/register", controller.registerUser);
 router.post("/login", controller.loginUser);
 router.get("/profile", userAuth, controller.profile);
 router.delete("/delete", userAuth, controller.deleteUser);
-router.get("/:id?", controller.getUsers);
 
 module.exports = router;
