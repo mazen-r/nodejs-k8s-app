4e747ff73af712cbffa9efa7cd4960743cd66843
Mazen Ramadan <mazenramadan00@gmail.com>
2023-05-02 20:20:56 +0300

    Add comments relations

--- database/models/relations.js
+++ database/models/relations.js
@@ -1,13 +1,12 @@
-const  User = require('./user')
-const  Post = require('./post')
+const User = require('./user')
+const Post = require('./post')
+const Comment = require('./comment')
 
 User.hasMany(Post, { foreignKey: 'authorId', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+Post.hasMany(Comment, { foreignKey: 'postId', onDelete: 'CASCADE', onUpdate: 'CASCADE' });
+
 Post.belongsTo(User, { foreignKey: {name: 'authorId', allowNull:false} });
+Comment.belongsTo(Post, { foreignKey: {name: 'postId', allowNull:false} });
+Comment.belongsTo(User, { foreignKey: {name: 'authorId', allowNull:false} });
 
-
-
-// User.hasMany(Post, { onDelete: 'CASCADE', onUpdate: 'CASCADE' });
-// Post.belongsTo(User, { foreignKey: 'authorId' });
-
-
-module.exports = { User, Post }
+module.exports = { User, Post, Comment }
