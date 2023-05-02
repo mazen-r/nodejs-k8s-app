f43a8a90f76b18d14a16997ea01afcbde2e542a8
Mazen Ramadan <mazenramadan00@gmail.com>
2023-05-02 20:17:46 +0300

    Add user verification utils

--- utils/twilio.js
+++ utils/twilio.js
@@ -1,16 +1,21 @@
-const { TWILIO_SID, TWILIO_AUTH, TWILIO_NUMBER } = require('../config');
+const { TWILIO_SID, TWILIO_AUTH, TWILIO_NUMBER, TWILIO_SERVICE_SID } = require('../config');
 
 const twilio = require('twilio');
 
 const client = twilio(TWILIO_SID, TWILIO_AUTH);
-const verificationCode = Math.floor(100000 + Math.random() * 900000);
 
 const sendOTP = async (phoneNumber) => {
-    client.messages.create({
-        body: `Your verification code is ${verificationCode}`,
-        to: phoneNumber,
-        from: TWILIO_NUMBER,
-    });
+    await client.verify.v2.services(TWILIO_SERVICE_SID)
+        .verifications
+        .create({to: phoneNumber, channel: 'sms'})
 };
 
-module.exports = { sendOTP }
+const verifyCode = async (phoneNumber, verificationCode) => {
+    const verificationStatus = await client.verify.v2.services(TWILIO_SERVICE_SID)
+          .verificationChecks
+          .create({to: phoneNumber, code: verificationCode});
+    return verificationStatus.status
+
+}
+
+module.exports = { sendOTP, verifyCode }
