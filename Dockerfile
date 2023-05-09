0bafb429dbfcc4e55c3c843f8508e97a4e13f807
Mazen Ramadan <mazenramadan00@gmail.com>
2023-05-10 01:16:35 +0300

    chore: Delete old app files

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,10 @@ FROM node:18.12.1
 
 WORKDIR /app
 
-COPY package.json ./
+COPY swagger.yaml src/package.json src/package-lock.json ./
 
 RUN npm install
 
-COPY . .
+COPY src/ .
 
 CMD npm run start
--- middleware/rateLimiter.js
+++
@@ -1,9 +0,0 @@
-const rateLimiter = require('express-rate-limit');
-
-const limiter = rateLimiter({
-    max: 100,
-    windowMS: 5 * 60 * 1000,
-    message: "You can't make any more requests at this moment. Please try again later.",
-});
-    
-module.exports = limiter
