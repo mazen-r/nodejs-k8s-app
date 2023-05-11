cc6a7e627b11622bbbe7603acd18e3c534f9c9fa
Mazen Ramadan <mazenramadan00@gmail.com>
2023-05-11 12:44:46 +0300

    chore: Add missing chars

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -59,7 +59,7 @@ const updateComment = async (req, res, next) => {
             };
             return res.status(403).json({message: "You are not authorized"});
         };
-        return res.status(404).json({message: "No comments with this Id"});
+        return res.status(404).json({message: "No comments found with this Id"});
     } catch (err) {
         next(err);
     };
